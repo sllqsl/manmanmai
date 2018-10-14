2812be705a1e108a431643ad07abc1afc445711f
sll <222@qq.com>
2018-10-14 16:50:58 +0800

    优惠券-轮播效果完成

--- js/couponproduct.js
+++ js/couponproduct.js
@@ -2,23 +2,73 @@ $(function(){
   // 功能1：发送ajax请求获取数据渲染
   var couponid = getSearch('couponid');
   console.log(couponid);
- $.ajax({
-   url:'http://127.0.0.1:9090/api/getcouponproduct',
-   data:{
-    couponid:couponid
-   },
-   dataType:'json',
-   type:'get',
-   success:function( info ) {
-     console.log(info);
-     var str = template('tmp',info);
-     $('.coupon-list ul').html(str);
-   }
- })
-//  功能2：点击优惠券出现录播图轮播
-  
+  render();
+  function render(){
+    $.ajax({
+      url:'http://127.0.0.1:9090/api/getcouponproduct',
+      data:{
+       couponid:couponid
+      },
+      dataType:'json',
+      type:'get',
+      success:function( info ) {
+        console.log(info);
+        var str = template('tmp',info);
+        $('.coupon-list ul').html(str);
+      }
+    })
+  }
+ //  功能2：点击优惠券出现录播图轮播
+   $('.coupon-list').on('click','li',function(){
+     $('.modal').show();
+     //点击出现模态框
+     
+     //  console.log($(this).children().children('.pic').children()[0]);
+    //  var str = $(this).children().children('.pic').children()[0];
+     //点击当前的图片标签
+     var arr =[];
+     var index;
+     var $li = $(this);
+     var newArr = Array.prototype.slice.call($('.coupon-list li'));
+     console.log(newArr);
+     newArr.forEach(function(v,i){
+      //  console.log($(v));
+       arr.push($(v).children().children('.pic').children()[0].outerHTML);
+      //  console.log(arr);
+     })
+     index = $(this).attr('couponProductId');
+
+     //点击下一张图片标签
+     $('.img').html(arr[index]);
+     render();
+     //点击切换图片
+     $('.arr-left').click(function(){
+       index--;
+       if (index <0) {
+         index = arr.length -1;
+         $('.img').html(arr[index]);
+         render();
+       }
+       $('.img').html(arr[index]);
+       render();
+     })
+     $('.arr-right').click(function(){
+       index++;
+       if (index > arr.length -1) {
+        $('.img').html(arr[index]);
+        render();
+       }
+       $('.img').html(arr[index]);
+       render();
+     })
+    $('.img').click(function(){
+      $('.modal').hide();
+      //点击取消模态框
+      $(document).scrollTop(125*index); 
+    })
+   })
  
-// 专门通过传递的参数, 可以解析出地址栏的参数值
+ // 专门通过传递的参数, 可以解析出地址栏的参数值
 function getSearch( name ) {
   var search = location.search; // ?name=pp&age=18&desc=%E5%B8%85
 
